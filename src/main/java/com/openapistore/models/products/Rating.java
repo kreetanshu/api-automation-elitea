b28466c4a470320a7565321fdbd0aafd317c8c0c
Reetanshu Kumar <kreetanshu@gmail.com>
2026-02-26 09:10:58 +0530

    Create src/main/java/com/openapistore/models/products/Rating.java

---
+++ src/main/java/com/openapistore/models/products/Rating.java
@@ -0,0 +1,25 @@
+package com.openapistore.models.products;
+
+public class Rating {
+    private Double rate;
+    private Integer count;
+
+    public Rating() {
+    }
+
+    public Double getRate() {
+        return rate;
+    }
+
+    public void setRate(Double rate) {
+        this.rate = rate;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+}
