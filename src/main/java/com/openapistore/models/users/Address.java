08f41c3b107ae1bdf4ef6b84c6f56bcf6e29123a
Reetanshu Kumar <kreetanshu@gmail.com>
2026-02-26 09:11:24 +0530

    Create src/main/java/com/openapistore/models/users/Address.java

---
+++ src/main/java/com/openapistore/models/users/Address.java
@@ -0,0 +1,52 @@
+package com.openapistore.models.users;
+
+public class Address {
+    private String city;
+    private String street;
+    private Integer number;
+    private String zipcode;
+    private GeoLocation geolocation;
+
+    public Address() {
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public GeoLocation getGeolocation() {
+        return geolocation;
+    }
+
+    public void setGeolocation(GeoLocation geolocation) {
+        this.geolocation = geolocation;
+    }
+}
