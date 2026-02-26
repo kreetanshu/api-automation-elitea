0f2417f421dd5a847bb8b7a6a93046e1e6fbb73b
Reetanshu Kumar <kreetanshu@gmail.com>
2026-02-26 09:58:43 +0530

    Create src/main/java/com/openapistore/models/user/UserUpdateRequest.java

---
+++ src/main/java/com/openapistore/models/user/UserUpdateRequest.java
@@ -0,0 +1,34 @@
+package com.openapistore.models.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserUpdateRequest {
+  private String email;
+  private String username;
+
+  public UserUpdateRequest() {}
+
+  public UserUpdateRequest(String email, String username) {
+    this.email = email;
+    this.username = username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+}
