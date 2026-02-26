13405426df97596619064ac47ae18fe8e01084e9
Reetanshu Kumar <kreetanshu@gmail.com>
2026-02-26 09:58:38 +0530

    Create src/main/java/com/openapistore/models/user/Name.java

---
+++ src/main/java/com/openapistore/models/user/Name.java
@@ -0,0 +1,34 @@
+package com.openapistore.models.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Name {
+  private String firstname;
+  private String lastname;
+
+  public Name() {}
+
+  public Name(String firstname, String lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+}
