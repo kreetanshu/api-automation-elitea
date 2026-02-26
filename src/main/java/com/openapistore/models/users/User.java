3a3b101d3745aee50774287137717bd161f063fb
Reetanshu Kumar <kreetanshu@gmail.com>
2026-02-26 09:11:26 +0530

    Create src/main/java/com/openapistore/models/users/User.java

---
+++ src/main/java/com/openapistore/models/users/User.java
@@ -0,0 +1,73 @@
+package com.openapistore.models.users;
+
+/**
+ * User POJO used for /users endpoints.
+ */
+public class User {
+    private Integer id;
+    private String email;
+    private String username;
+    private String password;
+    private Name name;
+    private Address address;
+    private String phone;
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
