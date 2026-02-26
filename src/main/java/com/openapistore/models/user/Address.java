1751a2d5bbbd9fddf3c51d878d3b7dea466881e0
Reetanshu Kumar <kreetanshu@gmail.com>
2026-02-26 09:58:37 +0530

    Create src/main/java/com/openapistore/models/user/Address.java

---
+++ src/main/java/com/openapistore/models/user/Address.java
@@ -0,0 +1,64 @@
+package com.openapistore.models.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Address {
+  private String city;
+  private String street;
+  private Integer number;
+  private String zipcode;
+  private GeoLocation geolocation;
+
+  public Address() {}
+
+  public Address(String city, String street, Integer number, String zipcode, GeoLocation geolocation) {
+    this.city = city;
+    this.street = street;
+    this.number = number;
+    this.zipcode = zipcode;
+    this.geolocation = geolocation;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public String getStreet() {
+    return street;
+  }
+
+  public void setStreet(String street) {
+    this.street = street;
+  }
+
+  public Integer getNumber() {
+    return number;
+  }
+
+  public void setNumber(Integer number) {
+    this.number = number;
+  }
+
+  public String getZipcode() {
+    return zipcode;
+  }
+
+  public void setZipcode(String zipcode) {
+    this.zipcode = zipcode;
+  }
+
+  public GeoLocation getGeolocation() {
+    return geolocation;
+  }
+
+  public void setGeolocation(GeoLocation geolocation) {
+    this.geolocation = geolocation;
+  }
+}
