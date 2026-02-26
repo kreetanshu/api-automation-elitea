aea13e093f9de580fe1fab7a34fcb9a2734f643d
Reetanshu Kumar <kreetanshu@gmail.com>
2026-02-26 09:58:19 +0530

    Create src/main/java/com/openapistore/models/product/Rating.java

---
+++ src/main/java/com/openapistore/models/product/Rating.java
@@ -0,0 +1,27 @@
+package com.openapistore.models.product;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Rating {
+  private Double rate;
+  private Integer count;
+
+  public Rating() {}
+
+  public Double getRate() {
+    return rate;
+  }
+
+  public void setRate(Double rate) {
+    this.rate = rate;
+  }
+
+  public Integer getCount() {
+    return count;
+  }
+
+  public void setCount(Integer count) {
+    this.count = count;
+  }
+}
