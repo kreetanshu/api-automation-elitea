f56b8c3548840684ad4d92a0418a4d5b58307950
Reetanshu Kumar <kreetanshu@gmail.com>
2026-02-26 09:11:01 +0530

    Create src/main/java/com/openapistore/models/carts/CartItem.java

---
+++ src/main/java/com/openapistore/models/carts/CartItem.java
@@ -0,0 +1,30 @@
+package com.openapistore.models.carts;
+
+public class CartItem {
+    private Integer productId;
+    private Integer quantity;
+
+    public CartItem() {
+    }
+
+    public CartItem(Integer productId, Integer quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
