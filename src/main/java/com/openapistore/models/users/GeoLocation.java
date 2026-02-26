e8f5626824ff615f4749ac740202e2d1042bd63c
Reetanshu Kumar <kreetanshu@gmail.com>
2026-02-26 09:11:04 +0530

    Create src/main/java/com/openapistore/models/users/GeoLocation.java

---
+++ src/main/java/com/openapistore/models/users/GeoLocation.java
@@ -0,0 +1,25 @@
+package com.openapistore.models.users;
+
+public class GeoLocation {
+    private String lat;
+    private String "long";
+
+    public GeoLocation() {
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLong() {
+        return "long";
+    }
+
+    public void setLong(String longitude) {
+        this."long" = longitude;
+    }
+}
